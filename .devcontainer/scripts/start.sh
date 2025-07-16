c6a796d8ebe1549254f351b72535d86ad670e61a
Dhruv Shah <dhruvshah1093@gmail.com>
2025-07-16 07:11:11 +0000

    Docker ready codespace

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -8,6 +8,9 @@ echo "Working directory set to $(pwd)"
 
 REPO_NAME=$(echo $GITHUB_REPOSITORY | cut -d'/' -f2)
 
+# Install Python dependencies
+pip install --no-cache-dir -r requirements.txt
+
 # Check if manage.py exists
 if [ ! -f manage.py ]; then
   echo "manage.py not found. Initializing Django project..."
