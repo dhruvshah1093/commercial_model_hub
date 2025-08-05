41a9b9a2af3abfb56126972fff449f24ed829864
Dhruv Shah <dhruvshah1093@gmail.com>
2025-08-05 07:18:34 +0000

    tests: begin

--- .devcontainer/scripts/enter_workspace.sh
+++ .devcontainer/scripts/enter_workspace.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker exec it $WORKSPACE_NAME-app-1 bash
-
-cd /workspaces/$WORKSPACE_NAME
+docker exec -it "$WORKSPACE_NAME-app-1" bash -c "cd /workspaces/$WORKSPACE_NAME && exec bash"
